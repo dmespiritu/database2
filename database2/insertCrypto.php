aa8381094b80dbc9489825bd454e1b1d5c040558
dmespiritu <dmespiritu@gmail.com>
2018-03-28 23:31:31 -0400

    Updating User Pages

--- database2/insertCrypto.php
+++ database2/insertCrypto.php
@@ -3,22 +3,22 @@
 include ("session.php");
 session_start();
  
-$database = "SneakerGang";
-$table = "Shoes";
+$database = "BlazeCoin";
+$table = "Blazecoin";
 
 if ($db){
 
-$insertShoes = "INSERT INTO $table (Name, Price, Color, Weight, UpperHeight, Size, Quantity) 
-VALUES ('$_POST[name]','$_POST[price]', '$_POST[color]','$_POST[weight]', '$_POST[upperHeight]', '10', '2')";
-mysqli_query($db,$insertShoes);
+$insertCrypto = "INSERT INTO $table (Name, Price) 
+VALUES ('$_POST[Name]','$_POST[Price]')";
+mysqli_query($db,$insertCrypto);
 
-$insertShoes = "INSERT INTO $table (Name, Price, Color, Weight, UpperHeight, Size, Quantity) 
-VALUES ('$_POST[name]','$_POST[price]', '$_POST[color]','$_POST[weight]', '$_POST[upperHeight]', '10.5', '2')";
-mysqli_query($db,$insertShoes);
+$insertCrypto = "INSERT INTO $table (Name, Price,) 
+VALUES ('$_POST[Name]','$_POST[Price]',)";
+mysqli_query($db,$insertCrypto);
 
-$insertShoes = "INSERT INTO $table (Name, Price, Color, Weight, UpperHeight, Size, Quantity) 
-VALUES ('$_POST[name]','$_POST[price]', '$_POST[color]','$_POST[weight]', '$_POST[upperHeight]', '11', '2')";
-mysqli_query($db,$insertShoes);
+$insertCrypto = "INSERT INTO $table (Name, Price)
+VALUES ('$_POST[Name]','$_POST[Price]')";
+mysqli_query($db,$insertCrypto);
 
 
 header("Location: admin.php");
